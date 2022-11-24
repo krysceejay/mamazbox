361fdf14a2e93b6904ded26f23e7feead2b3bde2
krys <mac@macs-MacBook-Pro.local>
2022-11-24 20:59:51 +0100

    home page responsive

--- components/layouts/Main.tsx
+++ components/layouts/Main.tsx
@@ -20,13 +20,31 @@ const Main: NextPage<Props> = ({ children, title }) => {
       </Head>
       <nav className="sticky top-0 z-40 shadow-sm w-full bg-white">
         <div className="bg-main-color text-center py-2 text-sm text-white w-full">Free shipping over $100 with code <span className="font-semibold">MAMAZBOX</span></div>
-        <div className="flex items-center justify-between container py-2">
+        <div className="flex items-center justify-between container py-2 sm:py-3">
           <div>
-            <div className="h-20 w-24">
+            <div className="h-14 w-14 sm:h-16 sm:w-20">
               <img className="w-full h-full object-cover" src="/assets/img/DarkPurple.png" />
             </div>
-            <span className="text-xs">Gifts for Expectant & New Moms</span>
+            {/* <span className="text-xs">Gifts for Expectant & New Moms</span> */}
           </div>
+          <ul className="hidden sm:flex">
+            <li className="px-5 py-1">
+              <a href="" className="uppercase font-medium hover:text-main-color">Home</a>
+            </li>
+            <li className="px-5 py-1">
+            <a href="" className="uppercase font-medium hover:text-main-color">Shop</a>
+            </li>
+            <li className="px-5 py-1">
+            <a href="" className="uppercase font-medium hover:text-main-color">Profile</a>
+            </li>
+            <li className="px-5 py-1">
+            <a href="" className="uppercase font-medium hover:text-main-color">Account</a>
+            </li>
+          </ul>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="block sm:hidden w-10 h-10">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+
         </div>
       </nav>
       <main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,12 @@ import Layout from '../components/layouts/Main'
 const Home: NextPage = () => {
   return (
     <Layout title="Mamazbox">
-      <section className="w-full h-[660px] bg-banner-img bg-no-repeat bg-cover bg-center relative">
+      <section className="w-full h-[560px] md:h-[660px] bg-banner-img bg-no-repeat bg-cover bg-center relative">
         <div className="overlay">
           <div className="container h-full w-full relative">
             <div className="absolute top-20 bottom-20 left-4 right-4 flex flex-col justify-center items-center">
-              <p className="text-[33px] text-[#fbf4f4]">Premium maternity and postpartum gifts</p>
-              <p className="text-[21px] text-[#fbf4f4] mt-4 italic">
+              <p className="text-2xl md:text-3xl text-[#fbf4f4] text-center">Premium maternity and postpartum gifts</p>
+              <p className="md:text-xl text-[#fbf4f4] mt-4 italic text-center">
                 Carefully and thoughtfully curated self-care gifts just for mama! Because mama should be pampered!
               </p>
               <a className="btn btn-banner mt-12 font-semibold text-sm tracking-widest cursor-pointer">
@@ -23,17 +23,17 @@ const Home: NextPage = () => {
       </section>
       <section>
         <div className="container">
-          <div className="flex my-16">
-            <div className="flex-1 pr-8">
-              <h2 className="text-[32px] tracking-wider font-medium">LUXURY MATERNITY GIFTS</h2>
-              <p className="text-lg leading-9 mt-4">
+          <div className="flex flex-col-reverse md:flex-row my-16">
+            <div className="flex-1 md:pr-8">
+              <h2 className="text-2xl mt-8 md:mt-0 md:text-3xl tracking-wider font-medium">LUXURY MATERNITY GIFTS</h2>
+              <p className="md:text-lg leading-9 mt-4">
               As mama is carried away with expecting and caring for baby, give her something that is just for her. 
               In mamazbox is enclosed essentials for self-care to boost mamas’ confidence through the pregnancy and post-partum journey, 
               helping her feel her best inside and out. Pick one of our carefully selected premium boxes for mama or give yourself a treat if you are the one expecting because you deserve it!  
               </p>
-              <p className="text-lg leading-9 mt-4"> We know the journey and different phases can take a toll on mama, and for this we want her to feel good in and out.</p>
+              <p className="md:text-lg leading-9 mt-4"> We know the journey and different phases can take a toll on mama, and for this we want her to feel good in and out.</p>
             </div>
-            <div className="flex-1 w-full h-[600px]">
+            <div className="md:flex-1 w-full h-[350px] md:h-[600px]">
               <img className="w-full h-full object-cover rounded-lg" src="/assets/img/img2.jpg" />
             </div>
           </div>
@@ -42,16 +42,16 @@ const Home: NextPage = () => {
       <section className="bg-gray-50">
         <div className="container">
           <div className="my-16">
-            <h2 className="text-[30px] tracking-wider font-medium leading-none">GIVE MAMA THE GIFTS SHE NEEDS</h2>
-            <div className="grid grid-rows-4 grid-flow-col gap-8 h-[800px] mt-12">
-              <div className="row-span-4">
-                <img className="w-full h-full object-cover rounded-lg" src="/assets/img/img7.jpg" />
+            <h2 className="text-2xl md:text-3xl tracking-wider font-medium leading-none">GIVE MAMA THE GIFTS SHE NEEDS</h2>
+            <div className="block md:grid grid-rows-4 grid-flow-col gap-8 md:h-[800px] mt-10">
+              <div className="md:row-span-4">
+                <img className="w-full h-[400px] md:h-full object-cover rounded-lg" src="/assets/img/img7.jpg" />
               </div>
-              <div className="row-span-2">
-                <img className="w-full h-full object-cover rounded-lg" src="/assets/img/img3.jpg" />
+              <div className="mt-8 md:mt-0 md:row-span-2">
+                <img className="w-full h-[400px] md:h-full object-cover rounded-lg" src="/assets/img/img3.jpg" />
               </div>
-              <div className="row-span-2">
-                <img className="w-full h-full object-cover rounded-lg" src="/assets/img/img1.jpg" />
+              <div className="mt-8 md:mt-0 md:row-span-2">
+                <img className="w-full h-[400px] md:h-full object-cover rounded-lg" src="/assets/img/img1.jpg" />
               </div>
             </div>
           </div>
@@ -61,30 +61,30 @@ const Home: NextPage = () => {
         <div className="container">
           <div className="my-16">
             <div className="flex justify-between items-center">
-              <h2 className="text-[30px] tracking-wider font-medium leading-none">FEATURED ITEMS</h2>
-              <a className="btn bg-main-color text-white py-3 px-6 font-semibold text-sm cursor-pointer">
+              <h2 className="text-2xl md:text-3xl tracking-wider font-medium leading-none">FEATURED ITEMS</h2>
+              <a className="btn bg-main-color text-white py-2 px-3 font-semibold text-xs cursor-pointer">
                 View All
               </a>
             </div>
-            <div className="grid grid-cols-3 gap-8 mt-12">
+            <div className="block sm:grid sm:grid-cols-3 sm:gap-4 md:gap-8 mt-12">
               <div>
-                <img className="w-full h-96 object-cover rounded-md" src="/assets/img/IMGP8661.jpg" />
+                <img className="w-full h-72 md:h-80 lg:h-96 object-cover rounded-md" src="/assets/img/IMGP8661.jpg" />
                 <div className="mt-4">
-                  <div className="text-xl font-light">The Spa Mini Gift Box for Mom</div>
+                  <div className="text-xl sm:text-base md:text-xl font-light">The Spa Mini Gift Box for Mom</div>
                   <span className="block text-2xl font-semibold">£68.00</span>
                 </div>
               </div>
-              <div>
-                <img className="w-full h-96 object-cover rounded-md" src="/assets/img/IMGP8697.jpg" />
+              <div className="mt-10 sm:mt-0">
+                <img className="w-full h-72 md:h-80 lg:h-96 object-cover rounded-md" src="/assets/img/IMGP8697.jpg" />
                 <div className="mt-4">
-                  <div className="text-xl font-light">Thank You Mom Gift Box</div>
+                  <div className="text-xl sm:text-base md:text-xl font-light">Thank You Mom Gift Box</div>
                   <span className="block text-2xl font-semibold">£68.00</span>
                 </div>
               </div>
-              <div>
-                <img className="w-full h-96 object-cover rounded-md" src="/assets/img/IMGP8717.jpg" />
+              <div className="mt-10 sm:mt-0">
+                <img className="w-full h-72 md:h-80 lg:h-96 object-cover rounded-md" src="/assets/img/IMGP8717.jpg" />
                 <div className="mt-4">
-                  <div className="text-xl font-light">Congratulations Mom Gift Box</div>
+                  <div className="text-xl sm:text-base md:text-xl font-light">Congratulations Mom Gift Box</div>
                   <span className="block text-2xl font-semibold">£68.00</span>
                 </div>
               </div>
@@ -94,18 +94,17 @@ const Home: NextPage = () => {
       </section>
       <section className="bg-gray-50">
         <div className="container">
-          <div className="flex my-16">
-            <div className="flex-1 w-full h-[600px]">
+          <div className="flex flex-col md:flex-row my-16">
+            <div className="md:flex-1 w-full h-[350px] md:h-[600px]">
               <img className="w-full h-full object-cover rounded-lg" src="/assets/img/img6.jpg" />
             </div>
-            <div className="flex-1 pl-8">
-              <h2 className="text-[32px] tracking-wider font-medium">SELECTED BY MUMS</h2>
-              <p className="text-lg leading-9 mt-4">
+            <div className="flex-1 md:pl-8">
+              <h2 className="text-2xl mt-8 md:mt-0 md:text-3xl tracking-wider font-medium">SELECTED BY MUMS</h2>
+              <p className="md:text-lg leading-9 mt-4">
               Just for you mama, because you should be pampered and deserve it. You have done what only you can do, birthing your baby. Everyone can be carried away with gifting baby, but mama should be thought of too. It can be difficult getting the right gifts for a mum to be or a new mum. We have made this easy for you by sourcing the best clean body and beauty products. Every single product has been carefully sourced, curated by us mums. 
               Send a new mum or mum to be our beautiful premium boxes, and let her know you are thinking of her. Mamazbox, mum tested, mum approved!  
               </p>
             </div>
-            
           </div>
         </div>
       </section>
@@ -113,10 +112,10 @@ const Home: NextPage = () => {
         <div className="overlay">
           <div className="container relative h-full w-full">
             <div className="bg-white text-center absolute top-40 bottom-40 left-4 right-4 flex flex-col justify-center items-center">
-              <h2 className="text-[32px] tracking-widest uppercase">Join mailing list</h2>
-              <form className="w-3/5 mx-auto mt-8">
+              <h2 className="text-2xl mt-8 md:mt-0 md:text-3xl tracking-wider uppercase">Join mailing list</h2>
+              <form className="mx-auto mt-8 w-4/5 sm:w-3/4">
                 <div className="flex flex-wrap">
-                  <div className="w-1/3 px-3">
+                  <div className="w-full md:w-1/2 lg:w-1/3 px-3">
                     <label className="block text-sm text-left mb-2">
                         First name
                     </label>
@@ -124,7 +123,7 @@ const Home: NextPage = () => {
                     className="appearance-none text-sm block w-full border focus:outline-none rounded py-2 h-12 px-4 mb-3"
                     type="text" />
                   </div>
-                  <div className="w-1/3 px-3">
+                  <div className="w-full md:w-1/2 lg:w-1/3 px-3">
                     <label className="block text-sm text-left mb-2">
                           Last name
                       </label>
@@ -132,7 +131,7 @@ const Home: NextPage = () => {
                       className="appearance-none text-sm block w-full border focus:outline-none rounded py-2 h-12 px-4 mb-3"
                       type="text" />
                   </div>
-                  <div className="w-1/3 px-3">
+                  <div className="w-full md:w-1/2 lg:w-1/3 px-3">
                       <label className="block text-sm text-left mb-2">
                           Email
                       </label>
